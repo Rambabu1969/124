d2eef5072a493654cd993663aa911b3af01d5a3a
Dr. Rambabu Palaka <66299153+Rambabu1969@users.noreply.github.com>
2022-10-25 12:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+//npm install express
+var express = require('express');
+var app = express();
+
+// set port, listen for requests
+const PORT = 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+// Default Route
+app.get('/', function (req, res) {
+   res.send('<h1>Hello World from 124</h1>');
+})
